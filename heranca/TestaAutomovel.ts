145b67f39f2c09d7ed5d64635ad21c14f0e45cb7
Gabrielli <gabivirgilio@oultook.com>
2024-07-05 10:27:46 -0300

    Aula Heranças

---
+++ heranca/TestaAutomovel.ts
@@ -0,0 +1,17 @@
+import { Terrestre } from "./Terrestre";
+import { Transporte } from "./Transporte";
+import { Automovel } from "./Automovel";
+
+
+const t1: Transporte = new Transporte(5);
+t1.visualizar();
+
+console.log("\n")
+
+const tr1: Terrestre = new Terrestre(5, 4, 250);
+tr1.visualizar();
+
+console.log("\n")
+
+const aut1: Automovel = new Automovel(5, 4, 300, "Azul", 4, "GEN-4587", 6 );
+aut1.visualizar();
---
+++ heranca/Transporte.ts
@@ -0,0 +1,26 @@
+export class Transporte{
+
+    private _capacidade: number;
+
+
+	constructor(capacidade: number) {
+		this._capacidade = capacidade;
+	}
+
+
+	public get capacidade(): number {
+		return this._capacidade;
+	}
+
+	public set capacidade(value: number) {
+		this._capacidade = value;
+	}
+  
+    public visualizar(){
+        console.log("***************************************")
+        console.log("Dados do meio de Transporte");
+        console.log("***************************************")
+        console.log(`Capacidade: ${this._capacidade}`)
+    }
+}
+
