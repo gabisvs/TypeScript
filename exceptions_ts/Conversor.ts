3d7c0cfbf5e720bd6646b7747a1f16b464ef860f
Gabrielli <gabivirgilio@oultook.com>
2024-07-10 14:44:56 -0300

    Aula Tratamento de Erros

---
+++ exceptions_ts/Conversor.ts
@@ -0,0 +1,20 @@
+﻿
+try{
+
+const resultado = converte(123);
+
+console.log(resultado);
+
+}catch(error: any) {
+    if (error instanceof TypeError)
+        console.error("\nVocê digitou um número. Digite um texto e tente novamente! ");
+    else
+        console.log("\nErro inesperado! Contate o aministrador do sistema. ")
+}finally{
+    console.log("\nFim do código! ")
+}          
+
+export function converte(conteudo: any): string {
+    return conteudo.toUpperCase();
+}
+
